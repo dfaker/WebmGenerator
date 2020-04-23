0c439c15d73a5a9107524ae5d5850194ce9f75ae
dfaker <35278260+dfaker@users.noreply.github.com>
2020-04-23 18:45:14 +0100

    Add files via upload

--- webmGenerator.py
+++ webmGenerator.py
@@ -1,11 +1,23 @@
 
+import os
 import sys
 
 import fileScan
 import interface
 import encoder
 
+if getattr(sys, 'frozen', False):
+  application_path = os.path.dirname(sys.executable)
+else:
+  application_path = os.path.dirname(os.path.realpath(__file__))
+
+os.chdir(application_path)
+os.environ["PATH"] = application_path + os.pathsep + os.environ["PATH"]
+
 videoFiles    = fileScan.gatherArgs(sys.argv[1:])
+
+print(videoFiles)
+
 selectedClips = interface.selectClips(videoFiles)
 print(selectedClips)
 encoder.processClips(selectedClips)

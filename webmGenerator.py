259da59cd1427b83b3d8c50e3c0398c7b111a45b
dfaker <35278260+dfaker@users.noreply.github.com>
2020-08-23 19:14:16 +0100

    Add files via upload

--- webmGenerator.py
+++ webmGenerator.py
@@ -1,13 +1,9 @@
 
 import sys
 from webmGeneratorController import WebmGeneratorController
-import mpv
 import threading
 from tkinter import Tk
 import tkinter.ttk as ttk
-
-print(threading.current_thread())
-print(mpv)
 
 initialFiles = sys.argv[1:]
 try:

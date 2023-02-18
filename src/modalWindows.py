910005e9b35786981942a2f95c7d9b16fbbe2dd8
DepFA <35278260+dfaker@users.noreply.github.com>
2023-02-18 23:20:50 +0000

    check parent path for mpv dll load

--- src/modalWindows.py
+++ src/modalWindows.py
@@ -21,7 +21,7 @@ from datetime import datetime
 try:
   scriptPath = os.path.dirname(os.path.abspath(__file__))
   
-  parentScriptPath = os.path.split( os.path.dirname(os.path.abspath(__file__)))
+  parentScriptPath = os.path.split( os.path.dirname(os.path.abspath(__file__)))[0]
 
   basescriptPath = os.path.split(scriptPath)[0]
   scriptPath_frozen = os.path.dirname(os.path.abspath(sys.executable))
